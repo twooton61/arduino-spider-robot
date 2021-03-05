9f3d5b7736f4a05b163dab930ff7606578846f2f
Terry Wooton <terry@oodle.io>
2021-03-04 21:32:37 -0500

    all rotates

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-#define SERVOMIN  125  // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575  // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  110  // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  625  // this is the 'maximum' pulse length count (out of 4096)
 
 // You can use this function if you'd like to set the pulse length in seconds
 // e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
@@ -33,18 +33,26 @@ void setup()
   pwm.setPWMFreq(60);
 }
 
-int angleToPulse(int ang){
+int angleToPulse(int ang)
+{
   return map(ang, 0, 180, SERVOMIN, SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max
+}
+
+void set_all(int deg)
+{
+  for (int i = 0; i < 12; i++) {
+    pwm.setPWM(i, 0, angleToPulse(deg));
+  }
 }
 
 void loop()
 {
-  pwm.setPWM(0, 0, angleToPulse(0));
+  for (int i = 0; i < 12; i++) {
+    set_all(0);
+    delay(3000);
 
-  delay(1000);
-
-  pwm.setPWM(0, 0, angleToPulse(180));
-
-  delay(1000);
+    set_all(180);
+    delay(3000);
+  }
 }
 
