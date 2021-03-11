0024119aad65109c9a1cff5f6c427ac465c35929
Terry Wooton <terry@oodle.io>
2021-03-10 21:21:34 -0500

    stands up

--- src/main.cpp
+++ src/main.cpp
@@ -38,21 +38,39 @@ int angleToPulse(int ang)
   return map(ang, 0, 180, SERVOMIN, SERVOMAX);  // map angle of 0 to 180 to Servo min and Servo max
 }
 
+void set_deg(int servo, int deg)
+{
+  pwm.setPWM(servo, 0, angleToPulse(deg));
+}
+
 void set_all(int deg)
 {
   for (int i = 0; i < 12; i++) {
-    pwm.setPWM(i, 0, angleToPulse(deg));
+    set_deg(i, deg);
   }
 }
 
 void loop()
 {
-  for (int i = 0; i < 12; i++) {
-    set_all(0);
-    delay(3000);
 
-    set_all(180);
-    delay(3000);
-  }
+  // hips
+  set_deg(0, 90);
+  set_deg(3, 90);
+  set_deg(6, 90);
+  set_deg(9, 90);
+
+  // knees
+  set_deg(1, 180);
+  set_deg(4, 0);
+  set_deg(7, 180);
+  set_deg(10, 0);
+
+  // ankles
+  set_deg(2, 135);
+  set_deg(5, 45);
+  set_deg(8, 135);
+  set_deg(11, 45);
+
+  delay(3000);
 }
 
