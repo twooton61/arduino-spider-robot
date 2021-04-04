cd854c07a258c638f8bb2b6532c4e2ce3ff057ec
Terry Wooton <terry@oodle.io>
2021-04-03 20:41:46 -0400

    steps right

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <Helpers.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
+#include <IRremote.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  110  // this is the 'minimum' pulse length count (out of 4096)
@@ -40,10 +41,15 @@ volatile int target_angles[MAX_SERVOS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 int signal_joint = 0;
 int last_servo_tick = 0;
-int last_keyboard_read_tick = 0;
+int last_input_read_tick = 0;
 
 const int SERVO_TICK = 20;
 const int DEGREES_PER_TICK = 20;
+
+IRrecv ir_receiver(2);
+decode_results ir_receiver_results;
+typedef unsigned long IRCode;
+IRCode last_ir_code = 0;
 
 int angleToPulse(int ang)
 {
@@ -52,7 +58,8 @@ int angleToPulse(int ang)
 
 void set_deg(int servo, int deg)
 {
-  target_angles[servo] = deg;
+  current_angles[servo] = deg;
+  pwm.setPWM(servo, 0, angleToPulse(deg));
 }
 
 void set_all(int deg)
@@ -61,7 +68,6 @@ void set_all(int deg)
     set_deg(i, deg);
   }
 }
-
 
 inline void low_base()
 {
@@ -102,103 +108,179 @@ inline void high_base()
   set_deg(BACK_RIGHT_ANKLE, 90);
 }
 
+IRCode get_receiver_code()
+{
+  if (!ir_receiver.decode(&ir_receiver_results)) {
+    return 0;
+  }
+
+  IRCode ir_code = ir_receiver_results.value;
+  IRCode ir_code_to_consider = 0;
+
+  const bool is_holding_button = ir_code == 4294967295;
+  if (is_holding_button) {
+    ir_code_to_consider = last_ir_code;
+  }
+  else {
+    ir_code_to_consider = ir_code;
+    last_ir_code = ir_code;
+  }
+
+  Serial.print("IR code: ");
+  Serial.print(ir_code);
+  Serial.print(" IR to consider: ");
+  Serial.println(String(ir_code_to_consider));
+
+  ir_receiver.resume();
+
+  last_ir_code = 0;
+
+  return ir_code_to_consider;
+}
+
+void base_hips()
+{
+  set_deg(FRONT_RIGHT_HIP, 135);
+  set_deg(FRONT_LEFT_HIP, 45);
+  set_deg(BACK_LEFT_HIP, 135);
+  set_deg(BACK_RIGHT_HIP, 45);
+}
+
 void setup()
 {
   Serial.begin(9600);
 
   Serial.println("setup");
 
+  ir_receiver.enableIRIn();
+
   pwm.begin();
   pwm.setPWMFreq(60);
 
-  set_deg(FRONT_RIGHT_HIP, 135);
-  set_deg(FRONT_LEFT_HIP, 45);
-  set_deg(BACK_LEFT_HIP, 135);
-  set_deg(BACK_RIGHT_HIP, 45);
+  base_hips();
 
   low_base();
 }
 
-const int DEFAULT_KEYBOARD_CODE = -1;
-int keyboard_code = DEFAULT_KEYBOARD_CODE;
-
 void loop()
 {
   const int latest_millies =  millis();
-  if (latest_millies > (last_servo_tick + SERVO_TICK)) {
-    last_servo_tick = latest_millies;
 
-    for (int servo_index = 0; servo_index < MAX_SERVOS; servo_index++) {
-      if (current_angles[servo_index] != target_angles[servo_index]) {
-        Serial.print("Servo: ");
-        Serial.print(servo_index);
-        Serial.print(" , current: ");
-        Serial.print(current_angles[servo_index]);
-        Serial.print(" , target: ");
-        Serial.print(target_angles[servo_index]);
-        // current_angles[servo_index] += SERVO_TICK;
-        current_angles[servo_index] = target_angles[servo_index];
-        if (current_angles[servo_index] > target_angles[servo_index]) {
-          current_angles[servo_index] = target_angles[servo_index];
-        }
+  if (latest_millies > (last_input_read_tick + 1000)) {
+    IRCode ir_code = get_receiver_code();
 
-        Serial.print(" , new: ");
-        Serial.println(current_angles[servo_index]);
-      }
+    switch ( ir_code ){
+      // all hips
+      case 0xFFA25D:
+      Serial.println("CH-");
+      low_base();
+      break;
 
-      pwm.setPWM(servo_index, 0, angleToPulse(target_angles[servo_index]));
-    }
-  }
+      case 0xFF629D:
+      mid_base();
+      break;
 
-  if (latest_millies > (last_keyboard_read_tick + 1000)) {
-    Serial.println("keyboard tick");
-    last_keyboard_read_tick = latest_millies;
-    int potential_keyboard_code = Serial.read();
-    if (potential_keyboard_code != DEFAULT_KEYBOARD_CODE) {
-      keyboard_code = potential_keyboard_code;
-    }
+      case 0xFFE21D:
+      high_base();
+      break;
 
-    Serial.print("received:");
-    Serial.println(keyboard_code, DEC);
+      // all knees
+      case 0xFF22DD:
+      Serial.println("|<<");
+      base_hips();
+      break;
 
-    switch (keyboard_code) {
-      // 2, low base
-      case 50:
-        low_base();
-        break;
-      // 5, tippy toe
-      case 53:
-        mid_base();
-        break;
-      // 8, tippy toe
-      case 56:
-        high_base();
-        break;
-      // 7, front left leg
-      case 55:
-        signal_joint = FRONT_LEFT_LEG_STARTING_JOINT;
-        break;
-      // 9, front right leg
-      case 57:
-        signal_joint = FRONT_RIGHT_LEG_STARTING_JOINT;
-        break;
-      // 1, back left
-      case 49:
-        signal_joint = BACK_LEFT_LEG_STARTING_JOINT;
-        break;
-      // 3, back right
-      case 51:
-        signal_joint = BACK_RIGHT_LEG_STARTING_JOINT;
-        break;
-      // <-
-      case 68:
-        break;
-      // v
-      case 66:
-        break;
-      // ->
-      case 67:
-        break;
+      case 0xFF02FD:
+      Serial.println(">>|");
+      break;
+
+      case 0xFFC23D:
+      Serial.println(">||");
+      break;
+
+      case 0xFFE01F:
+      Serial.println("-");
+      break;
+
+      case 0xFFA857:
+      Serial.println("+");
+      break;
+
+      case 0xFF906F:
+      Serial.println("EQ");
+      break;
+
+      // front left
+      case 0xFF6897:
+      Serial.println("0");
+      break;
+
+      case 0xFF9867:
+      Serial.println("100+");
+      break;
+
+      case 0xFFB04F:
+      Serial.println("200+");
+      break;
+
+      // front right
+      case 0xFF30CF:
+      Serial.println("1");
+      break;
+
+      case 0xFF18E7:
+      Serial.println("2");
+
+
+      set_deg(BACK_RIGHT_KNEE, 180);
+      delay(200);
+      set_deg(BACK_RIGHT_HIP, 90);
+      delay(200);
+      set_deg(BACK_RIGHT_KNEE, 135);
+      delay(200);
+
+      set_deg(FRONT_RIGHT_KNEE, 0);
+      delay(200);
+      set_deg(FRONT_RIGHT_HIP, 180);
+      delay(200);
+      set_deg(FRONT_RIGHT_KNEE, 45);
+      delay(200);
+
+      set_deg(BACK_RIGHT_HIP, 45);
+      delay(200);
+      set_deg(FRONT_RIGHT_HIP, 135);
+      delay(200);
+
+      break;
+
+      case 0xFF7A85:
+      Serial.println("3");
+
+      break;
+
+      case 0xFF10EF:
+      Serial.println("4");
+      break;
+
+      case 16726215:
+      Serial.println("5");
+      break;
+
+      case 0xFF5AA5:
+      Serial.println("6");
+      break;
+
+      case 0xFF42BD:
+      Serial.println("7");
+      break;
+
+      case 0xFF4AB5:
+      Serial.println("8");
+      break;
+
+      case 0xFF52AD:
+      Serial.println("9");
+      break;
     }
   }
 }
